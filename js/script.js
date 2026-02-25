5317a055a311d0b74e6e1278f152d90171440d65
papasquat55 <papasquat55@gmail.com>
2026-02-24 19:31:29 -0800

    Take 2 2-24-26

--- js/script.js
+++
@@ -1,4 +0,0 @@
-
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1132},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-about-author","displayName":"About Author","link":{"linkType":"LinkTypePage","href":"#!page-about-author"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-upcoming-and-available-books","displayName":"Upcoming and Available Books","link":{"linkType":"LinkTypePage","href":"#!page-upcoming-and-available-books"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"Contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
